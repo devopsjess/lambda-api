4c3fd0c2a37fbdc8c738b4abcde2ed378cdb7f77
Hnin <hwutyee@gmail.com>
2024-05-08 20:29:43 +0800

    get function

---
+++ index.test.js
@@ -0,0 +1,31 @@
+const { firsthandler } = require('./index')
+
+describe('firsthandler', () => {
+  it('should return status code 200 and the correct message', async () => {
+    const event = {};
+    const expectedResponse = {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: "This is get function!",
+      }, null, 2),
+    };
+    const result = await firsthandler(event);
+    expect(result).toEqual(expectedResponse);
+  });
+});
+
+describe('secondhandler', () => {
+    it('should return status code 200 and the correct message', async () => {
+      const event = {};
+      const expectedResponse = {
+        statusCode: 200,
+        body: JSON.stringify({
+          message: "This is post function!",
+        }, null, 2),
+      };
+      const result = await firsthandler(event);
+      expect(result).toEqual(expectedResponse);
+    });
+  });
+  
+  
